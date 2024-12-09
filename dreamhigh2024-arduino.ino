c0ff49c5fad9885b9fb859cc8cb3d2100d9c88ce
piz2a <43025513+piz2a@users.noreply.github.com>
2024-12-09 21:07:22 +0900

    init

---
+++ dreamhigh2024-arduino.ino
@@ -0,0 +1,118 @@
+#include <TFT_eSPI.h> // Hardware-specific library
+#include <AimHangul.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <Image_print.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClientSecure.h>
+
+const char* ssid     = "gbshs-rain 2.4G";  // The SSID (name) of the Wi-Fi network you want to connect to
+const char* password = "happygbs";         // The password of the Wi-Fi network
+
+String url = "https://raw.githubusercontent.com/piz2a/SnareStrokeGAN/refs/heads/master/record.txt";
+
+#define WHITE 0xFFFF
+#define GREEN 0x07E0
+#define RED 0xF800
+#define YELLOW 0xFFE0
+#define BLACK 000000
+
+TFT_eSPI tft = TFT_eSPI(); // Invoke library
+
+std::vector<std::string> scenes = {"Lobby", "thermo"}; // 필요하면 사용 - 근데 필요 없을 듯
+std::string scene = "Lobby";
+
+
+void setup() {
+  wifi_connect();
+  
+  tft.begin();
+  tft.setRotation(0);
+
+  new_scene(scene);
+}
+
+void loop() {
+  delay(3000);
+  scene = scenes[1];
+  new_scene(scene);
+  // 버튼 입력 들어오면 다음 장면으로 전환 - 더블클릭 구분
+}
+
+void wifi_connect() {
+  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
+  delay(10);
+  Serial.println('\n');
+  
+  WiFi.begin(ssid, password);             // Connect to the network
+  Serial.print("Connecting to ");
+  Serial.print(ssid); Serial.println(" ...");
+
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+    delay(1000);
+    Serial.print(++i); Serial.print(' ');
+  }
+
+  Serial.println('\n');
+  Serial.println("Connection established!");  
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
+}
+
+String get(String query) {
+  if (WiFi.status() == WL_CONNECTED) {
+    WiFiClientSecure client;
+    client.setInsecure();
+    
+    HTTPClient https;
+    String fullUrl = url + query;
+    Serial.println("Requesting " + fullUrl);
+    if (https.begin(client, fullUrl)) {
+      int httpCode = https.GET();
+      Serial.println("============== Response code: " + String(httpCode));
+      if (httpCode > 0) {
+        String result = https.getString();
+        Serial.println(result);
+      }
+      https.end();
+      return result;
+    } else {
+      Serial.printf("[HTTPS] Unable to connect\n");
+    }
+    return "{}";
+  }
+}
+
+void new_scene(std::string scene) {  //이 함수 내에 장면 구성, 이 함수를 호출하면 화면 변경
+  if (scene == std::string("Lobby")) {
+    tft.fillScreen(RED);
+  }
+  else if (scene == std::string("thermo")) {
+    tft.fillScreen(BLACK);
+    
+    tft.setCursor(0,0);
+    AimHangul_h2(20,5,"YY/MM/DD  DAY", WHITE);
+    tft.drawLine(0,25,240,25, WHITE);
+    tft.setCursor(10,210);
+    tft.setTextSize(2);
+    tft.println("Temp: __._'C");
+    tft.setCursor(10,235);
+    tft.println("Humidity: __._%");
+    tft.setCursor(10,260);
+    tft.println("Max Temp: __._'C");
+    tft.setCursor(10,285);
+    tft.println("Min_Temp: __._'C");
+    tft.setCursor(0,30);
+    tft.drawBitmap(0,0,sunny_image, 180, 180, TFT_WHITE);
+  }
+  else {
+    tft.fillScreen(BLACK);
+    AimHangul_x4(0,70, "장면 없음", WHITE);
+  }
+}
+
+// 이미지 출력 함수 - 이미지 저장 라이브러리 만들자~
